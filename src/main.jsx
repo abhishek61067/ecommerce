3bd62c4913eef5faf08b1c83305b055f361f0090
abhishek61067 <abhishek.bhattarai61067@gmail.com>
2025-07-10 15:12:16 +0545

    vite and chakra ui setup

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ChakraProvider></ChakraProvider>
+  </StrictMode>
+);
